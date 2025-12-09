886a5e297eedb924519d9a7d409a1286ada87f40
BastienIRD <bastien.grasset@ird.fr>
2025-12-09 08:34:22 +0000

    fix: folder creation

--- generate_rev2_paper.R
+++ generate_rev2_paper.R
@@ -5,9 +5,20 @@ purl_with_logs(here::here("Paper_WPDCS_COMP_FSJ_IOTC_Rev2.Rmd"),
                output = here::here("Paper_WPDCS_Comp_FSJ_IOTC_Rev2.R"),
                documentation = 2)
 }
+
+if(!dir.exists(here::here("outputs"))){
+  dir.create(here::here("outputs"))
+}
+if(!dir.exists(here::here("outputs/figs_final"))){
+  dir.create(here::here("outputs/figs_final"))
+}
+if(!dir.exists(here::here("outputs/figs"))){
+  dir.create(here::here("outputs/figs"))
+}
+
 source(here::here("Paper_WPDCS_Comp_FSJ_IOTC_Rev2.R")) # loading the data, mapping, filtering and creating all the outptus needed for the paper.
 
 bookdown::render_book(output_format = "bookdown::pdf_document2", output_dir = getwd(), 
-                      output_file = "Paper_WPDCS_COMP_FSJ_IOTC_Rev2", envir = new.env())
+                      output_file = here::here("Paper_WPDCS_COMP_FSJ_IOTC_Rev2"), envir = new.env())
 
-rmarkdown::render("WPDCS-oral-presentation-.Rmd")
+rmarkdown::render(here::here("WPDCS-oral-presentation-.Rmd"))
